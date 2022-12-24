2294b9bdb7c662b15920f8e7222f26dbcc8f11b7
Dawid Januszek <djanuszek@griddynamics.com>
2022-12-24 08:45:22 +0100

    added readme file

--- firmls.sh
+++ firmls.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-for line in $(cat /Users/dawid/Documents/Projects/firmpass/serial.txt); do
+for line in $(cat $HOME/Documents/firmls/serial.txt) 
+do
 	echo -n "GD-"$line | tr a-z A-Z | md5 | cut -c -8	
 done
